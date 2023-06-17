cc4fc9875964a402a03032a859a3c5f31df2c12d
Naftali Holland <naftaliholland01@gmail.com>
2023-06-17 20:15:37 +0300

    Added a dynamic path segment and captured values

--- Monthly_challenges/challenges/views.py
+++ Monthly_challenges/challenges/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
+
+def monthly_challenge(request, month):
+    myresponse = ""
+    if month == "January":
+        myresponse += "It's january"
+
+    elif month == "February":
+        myresponse += "It's february baiby"
+
+    else:
+        return HttpResponseNotFound("Page Not found")
+
+    return HttpResponse(myresponse)
