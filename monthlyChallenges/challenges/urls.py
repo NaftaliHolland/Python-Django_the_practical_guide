66296d458746f57df77bdcd457e17468cf052903
NaftaliHolland <124318361+NaftaliHolland@users.noreply.github.com>
2023-06-17 19:30:16 +0300

    Delete monthlyChallenges directory

--- monthlyChallenges/challenges/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-#list of urls to be supported and the function that should be trigered when the path is reached
-urlpatterns = [
-    path("january", views.index),
-    path("february", views.feb)
-]
