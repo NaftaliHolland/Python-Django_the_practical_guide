be66d86cc170df68478cf6e34308c95741d08c62
Naftali Holland <naftaliholland01@gmail.com>
2023-06-18 08:49:07 +0300

    Learnt and implemented path converters

--- Monthly_challenges/challenges/views.py
+++ Monthly_challenges/challenges/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+myresponse = ""
+def monthly_challenge_by_number(request, month):
+    if month == 1:
+        myresponse = "wazaa"
+    elif month == 2:
+        myresponse = "Feb baiby"
+    else:
+        return HttpResponseNotFound("We don't have that sorry")
+    return HttpResponse(myresponse)
+
 def monthly_challenge(request, month):
     myresponse = ""
     if month == "January":
