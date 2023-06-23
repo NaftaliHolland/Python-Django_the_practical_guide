e9f5d51660cdbb510663fe28bb5a1b098692c8a7
Naftali Holland <naftaliholland01@gmail.com>
2023-06-23 23:00:30 +0300

    Added more dynamic view logic using a dictionary with month : challenge pair

--- Monthly_challenges/challenges/views.py
+++ Monthly_challenges/challenges/views.py
@@ -2,17 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+monthly_challenges = {
+    "January" : "Make android apps",
+    "February" : "Start going to the gym twice a week",
+    "March" : "Eat meat",
+    "April" : "Read alot",
+    "May" : "Get a new laptop",
+    "June" : "Read more",
+    "July" : "Eat more meat",
+    "August" : "Definatelly not eat vegetables",
+    "September" : "What should I do ? Eat more meat",
+    "October" : "O don't know man",
+    "November" : "What to do",
+    "December" : "Make a plan for the next year"
+}
+
 myresponse = ""
 def monthly_challenge_by_number(request, month):
-    if month == 1:
-        myresponse = "wazaa"
-    elif month == 2:
-        myresponse = "Feb baiby"
-    else:
+    try:
+        myresponse = monthly_challenges[month]
+        return HttpResponse(myresponse)
+    except:
         return HttpResponseNotFound("We don't have that sorry")
-    return HttpResponse(myresponse)
 
-def monthly_challenge(request, month):
+'''def monthly_challenge(request, month):
     myresponse = ""
     if month == "January":
         myresponse += "It's january"
@@ -23,4 +36,4 @@ def monthly_challenge(request, month):
     else:
         return HttpResponseNotFound("Page Not found")
 
-    return HttpResponse(myresponse)
+    return HttpResponse(myresponse)'''
